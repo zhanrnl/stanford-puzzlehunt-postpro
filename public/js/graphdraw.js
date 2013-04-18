fdfbbaf42b6c86575aa6354d7253c7821795673f
Lennart Jansson <lennartj@stanford.edu>
2013-04-17 22:59:25 -0700

    Graph animation madness

--- public/js/graphdraw.js
+++ public/js/graphdraw.js
@@ -1,4 +1,6 @@
 $(function() {
+  $('.puzzleGraphBox').css('visibility','hidden');
+
   var centerCoords = function($elem) {
     var offset = $elem.position();
     var width = $elem.width();
@@ -22,18 +24,44 @@ $(function() {
     if (link.center2[1] > maxY) {
       maxY = link.center2[1];
     }
+    link.extOffset = Math.random();
   })
 
   var elem = $('#puzzleGraphContainer')[0];
   var paper = Raphael(elem, 1100, maxY);
 
-  $.map(links, function(link) {
-    var p = paper.path('M' + coordsToStr(link.center1) + 'L' + coordsToStr(link.center2))
-    .attr('stroke-width', '4');
-    if (link.unlocked) {
-      p.attr('stroke', '#fff');
-    } else {
-      p.attr('stroke', '#555').attr('stroke-dasharray', '.');
-    }
-  }); 
+  var paths = new Array(links.length);
+  var extensionAmount = -1;
+
+  $('.puzzleGraphBox').each(function(i, elem) {
+    setTimeout(function() {
+      $(elem).css('visibility','visible');
+    }, Math.random() * 1000);
+  });
+
+  setTimeout(function() {
+    var extending = setInterval(function() {
+      if (extensionAmount > 2) {
+        clearInterval(extending);
+      }
+      $.map(links, function(link, i) {
+        var locExt = extensionAmount + link.extOffset;
+        if (locExt > 1) locExt = 1;
+        else if (locExt < 0) locExt = 0;
+        var px = link.center1[0] + locExt * (link.center2[0] - link.center1[0]);
+        var py = link.center1[1] + locExt * (link.center2[1] - link.center1[1]);
+        if (paths[i] != null) {
+          paths[i].remove();
+        }
+        paths[i] = paper.path('M' + coordsToStr(link.center1) + 'L' + coordsToStr([px, py]))
+        .attr('stroke-width', '4');
+        if (link.unlocked) {
+          paths[i].attr('stroke', '#fff');
+        } else {
+          paths[i].attr('stroke', '#555').attr('stroke-dasharray', '.');
+        }
+      }); 
+      extensionAmount += 0.04;
+    }, 50);
+  }, 600);
 });
